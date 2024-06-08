9f15af9cb7ec17a8c9b2d5a608d9a1bc0ed3cc15
anitkumarsingh <anikumarsingh99@gmail.com>
2024-06-08 16:42:26 +0530

    post request

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,15 +1,32 @@
+import connect from "@/app/lib/db";
+import user from "@/app/lib/models/users";
+import { NextResponse } from "next/server";
 
-import connect from '@/app/lib/db';
-import user from '@/app/lib/models/users';
-import {NextResponse} from 'next/server';
-
-export const GET = async()=>{
-    try {
-        await connect();
-        const users = await user.find();
-        return new NextResponse(JSON.stringify({users,message:'Users fetched successfully!'}),{status:200})
-        
-    } catch (error:any) {
-      return new NextResponse('Error in fetching users',error.message);  
-    }
-}
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await user.find();
+    return new NextResponse(
+      JSON.stringify({ users, message: "Users fetched successfully!" }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users", error.message);
+  }
+};
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    await connect();
+    const newUser = new user(body);
+    await newUser.save();
+    return new NextResponse(
+      JSON.stringify({ message: "User is created!", newUser }),
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return new NextResponse(`Error in creating user ${error.message}`, {
+      status: 500,
+    });
+  }
+};
